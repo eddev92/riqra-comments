4bafb24f6afcdfb5966b24ed1874842fa4ce5d34
Eddev92 <edwarllanca@hotmail.com>
2019-06-27 19:28:17 -0500

    add main-content component and header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainContent from './components/main-content';
+
+function App() {
+  return (
+    <div className="App-header">
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main-content/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/main-content.css';
+
+const MainContent = ({ props }) => {
+    return (
+        <div className='main-content'>
+            
+        </div>
+    )
+}
+
+export default MainContent;
